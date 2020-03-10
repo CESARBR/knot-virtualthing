c050b20bc5caacdcfb77030266a61807693ff934
Lucas Cabral <lcc2@cesar.org.br>
2020-03-10 07:40:20 -0300

    Add tests for register state

--- tests/st-machine-test.c
+++ tests/st-machine-test.c
@@ -120,6 +120,97 @@ START_TEST(disconnected_get_next_event_reg_perm_is_disconnected)
 }
 END_TEST
 
+START_TEST(register_get_next_event_ready_is_register)
+{
+	int next_state = get_next_register(EVT_READY, NULL);
+	ck_assert_int_eq(next_state, ST_REGISTER);
+}
+END_TEST
+
+START_TEST(register_get_next_event_not_ready_is_disconnected)
+{
+	int next_state = get_next_register(EVT_NOT_READY, NULL);
+	ck_assert_int_eq(next_state, ST_DISCONNECTED);
+}
+END_TEST
+
+START_TEST(register_get_next_event_timeout_is_register)
+{
+	int next_state = get_next_register(EVT_TIMEOUT, NULL);
+	ck_assert_int_eq(next_state, ST_REGISTER);
+}
+END_TEST
+
+START_TEST(register_get_next_event_reg_ok_is_auth)
+{
+	int next_state = get_next_register(EVT_REG_OK, NULL);
+	ck_assert_int_eq(next_state, ST_AUTH);
+}
+END_TEST
+
+START_TEST(register_get_next_event_reg_not_ok_is_register)
+{
+	int next_state = get_next_register(EVT_REG_NOT_OK, NULL);
+	ck_assert_int_eq(next_state, ST_REGISTER);
+}
+END_TEST
+
+START_TEST(register_get_next_event_auth_ok_is_register)
+{
+	int next_state = get_next_register(EVT_AUTH_OK, NULL);
+	ck_assert_int_eq(next_state, ST_REGISTER);
+}
+END_TEST
+
+START_TEST(register_get_next_event_auth_not_ok_is_register)
+{
+	int next_state = get_next_register(EVT_AUTH_NOT_OK, NULL);
+	ck_assert_int_eq(next_state, ST_REGISTER);
+}
+END_TEST
+
+START_TEST(register_get_next_event_schema_ok_is_register)
+{
+	int next_state = get_next_register(EVT_SCH_OK, NULL);
+	ck_assert_int_eq(next_state, ST_REGISTER);
+}
+END_TEST
+
+START_TEST(register_get_next_event_schema_not_ok_is_register)
+{
+	int next_state = get_next_register(EVT_SCH_NOT_OK, NULL);
+	ck_assert_int_eq(next_state, ST_REGISTER);
+}
+END_TEST
+
+START_TEST(register_get_next_event_unreg_req_is_register)
+{
+	int next_state = get_next_register(EVT_UNREG_REQ, NULL);
+	ck_assert_int_eq(next_state, ST_REGISTER);
+}
+END_TEST
+
+START_TEST(register_get_next_event_data_update_is_register)
+{
+	int next_state = get_next_register(EVT_DATA_UPDT, NULL);
+	ck_assert_int_eq(next_state, ST_REGISTER);
+}
+END_TEST
+
+START_TEST(register_get_next_event_publish_data_is_register)
+{
+	int next_state = get_next_register(EVT_PUB_DATA, NULL);
+	ck_assert_int_eq(next_state, ST_REGISTER);
+}
+END_TEST
+
+START_TEST(register_get_next_event_reg_perm_is_register)
+{
+	int next_state = get_next_register(EVT_REG_PERM, NULL);
+	ck_assert_int_eq(next_state, ST_REGISTER);
+}
+END_TEST
+
 static void add_disconnected_state_test_case(Suite *sm_suite)
 {
 	TCase *tc_disconnected;
@@ -144,6 +235,29 @@ static void add_disconnected_state_test_case(Suite *sm_suite)
 	suite_add_tcase(sm_suite, tc_disconnected);
 }
 
+static void add_register_state_test_case(Suite *sm_suite)
+{
+	TCase *tc_register;
+
+	/* Register test case */
+	tc_register = tcase_create("Register");
+	tcase_add_test(tc_register, register_get_next_event_ready_is_register);
+	tcase_add_test(tc_register, register_get_next_event_not_ready_is_disconnected);
+	tcase_add_test(tc_register, register_get_next_event_timeout_is_register);
+	tcase_add_test(tc_register, register_get_next_event_reg_ok_is_auth);
+	tcase_add_test(tc_register, register_get_next_event_reg_not_ok_is_register);
+	tcase_add_test(tc_register, register_get_next_event_auth_ok_is_register);
+	tcase_add_test(tc_register, register_get_next_event_auth_not_ok_is_register);
+	tcase_add_test(tc_register, register_get_next_event_schema_ok_is_register);
+	tcase_add_test(tc_register, register_get_next_event_schema_not_ok_is_register);
+	tcase_add_test(tc_register, register_get_next_event_unreg_req_is_register);
+	tcase_add_test(tc_register, register_get_next_event_data_update_is_register);
+	tcase_add_test(tc_register, register_get_next_event_publish_data_is_register);
+	tcase_add_test(tc_register, register_get_next_event_reg_perm_is_register);
+
+	suite_add_tcase(sm_suite, tc_register);
+}
+
 Suite *state_machine_suite(void)
 {
 	Suite *sm_suite;
@@ -151,6 +265,7 @@ Suite *state_machine_suite(void)
 	sm_suite = suite_create("State Machine");
 
 	add_disconnected_state_test_case(sm_suite);
+	add_register_state_test_case(sm_suite);
 
 	return sm_suite;
 }
